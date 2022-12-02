a8d1cf66a865eee7372e9242b8242d9fc99ff469
Jacob-Duhaime <98777081+Jacob-Duhaime@users.noreply.github.com>
2022-12-02 16:09:37 -0500

    Update spellcheck.cpp

--- spellcheck.cpp
+++ spellcheck.cpp
@@ -1,7 +1,23 @@
 #include "spellcheck.h"
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 Spellcheck::Spellcheck(std::string user_input){
     // what the fortnite
-    this -> word = user_input;
+    this-> word = user_input;
+}
+
+void Spellcheck::fillDictionary(){
+
+    std::ifstream inFile(fileName);
+    std::string line;
+
+    while (std::getline(inFile, line)) {
+        dictionary.push_back(line);
+    }
+    inFile.close();
+    std::cout << dictionary[5] << std::endl;
 }
