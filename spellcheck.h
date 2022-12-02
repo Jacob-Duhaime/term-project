0d9ba71c2014df2e1bb79e071370871a1c788f5e
Jacob-Duhaime <98777081+Jacob-Duhaime@users.noreply.github.com>
2022-12-02 16:09:13 -0500

    Update spellcheck.h

--- spellcheck.h
+++ spellcheck.h
@@ -1,13 +1,14 @@
 #include <string>
+#include <vector>
 
 class Spellcheck{
     private:
         // private parts
         std::string word;
+        std::string fileName = "dictionary.txt";
+        std::vector<std::string> dictionary;
     public:
         // public parts
         Spellcheck(std::string user_input); //constructor
-        struct Node();
-        void insert(Node, string s);
-        
+        void fillDictionary();
 };
