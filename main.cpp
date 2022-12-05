90197a9eaafd7172083be8c9badfd01d42dfb4d4
nfaciano <98556750+nfaciano@users.noreply.github.com>
2022-12-05 14:45:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,20 +12,20 @@ int main(){
     
     //Call the spell check function and read the dictionary
     Spellcheck userWord(word);
-    std::vector<std::string> dict = userWord.fillDictionary();
+    std::vector<std::string> dict = userWord.fillDictionary(); 
 
-    Spellcheck::CreateNode* start_node = new Spellcheck::CreateNode;
+    Spellcheck::CreateNode* start_node = new Spellcheck::CreateNode; //create node using call to struct
 
     for (int i = 0; i < 1000; i++){
         if (dict[i] == word){
-            std::cout << word << std::endl;
+            std::cout << word << std::endl; //output word if single letter
             return 0;
         }
-        userWord.buildTree(start_node, dict[i]);
+        userWord.buildTree(start_node, dict[i]); //build tree
     }
 
     if (userWord.inDict(start_node, word) == true){
-        std::cout << word << std::endl;
+        std::cout << word << std::endl; //output word if word is in dictionary
     }
 
     
