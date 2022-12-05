092c63f1f496f301b0e41fba2e0b052de4025734
apichardo2v <71112824+apichardo2v@users.noreply.github.com>
2022-12-05 11:35:02 -0500

    Update spellcheck.h

--- spellcheck.h
+++ spellcheck.h
@@ -20,9 +20,13 @@ class Spellcheck{
             }
         };
         Spellcheck(std::string user_input); //constructor
+        //vector to fill the dictionary
         std::vector<std::string> fillDictionary();
+        //boolean to see if the word is in the dictionary that we provided
         bool inDict(Spellcheck::CreateNode* start_node, std::string ans);
+        //function to find the posibilities of the words
         void posibilities(Spellcheck::CreateNode* start_node, std::string ans);
+        //builds the trie that will search for the word in the dictionary database
         void buildTree(Spellcheck::CreateNode* start_node, std::string segment);
 
 };
